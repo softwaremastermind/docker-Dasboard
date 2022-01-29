e0516ba3ba84cc9d3ace4a0bb988ffd91942bd9b
Jan Reiss <jan@behead.de>
2022-01-29 20:50:22 +0100

    switched to event based ui update

--- app.go
+++ app.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
-	"encoding/json"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/gin-gonic/gin"
 )
+
+type Event struct {
+	ID      int
+	Message string
+}
+
+func streamEvents(c *gin.Context) {
+	listener := make(chan interface{})
+	go sendEvents(listener)
+	c.Stream(func(w io.Writer) bool {
+		event := <-listener
+		if event != nil {
+			c.SSEvent("message", event)
+			return true
+		}
+		return false
+	})
+}
 
 func main() {
 	ctx := context.Background()
@@ -28,8 +47,10 @@ func main() {
 		c.File("index.html")
 	})
 
+	r.GET("/stream", streamEvents)
+
 	r.GET("/status", func(c *gin.Context) {
-		containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+		containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
 		if err != nil {
 			panic(err)
 		}
@@ -43,4 +64,25 @@ func main() {
         int64(len(string(u))), gin.MIMEJSON, strings.NewReader(string(u)), nil)
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func sendEvents(listener chan interface{}) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	msgs, errs := cli.Events(ctx, types.EventsOptions{})
+	var i int
+	for {
+		i ++
+		select {
+			case err := <-errs:
+				fmt.Println(err)
+				listener <- Event{i, "update"}
+			case msg := <-msgs:
+				fmt.Println(msg)
+				listener <- Event{i, "update"}
+		}
+	}
 }
